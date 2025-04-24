a659daac73e56909400d86c1fd9203a557085fcc
ST10294145 <st10294145@vcconnect.edu.za>
2025-04-24 09:45:07 +0200

    User Entity class added
    User entity table has been added to capture user data.
    The data that will be captured is the name,email,password.
    This information will be on the register page.
    ID is auto-generated

--- app/src/main/java/com/fake/money_lock/data/User.kt
+++ app/src/main/java/com/fake/money_lock/data/User.kt
@@ -1,4 +1,13 @@
 package com.fake.money_lock.data
 
-class User {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "user_table")
+data class User(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int,
+    val name: String,
+    val email: String,   // ✅ Use String here
+    val password: String
+)
