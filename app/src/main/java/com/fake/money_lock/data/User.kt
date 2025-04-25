caba402741ef588e1adb8c375ca86099318f17d9
ST10294145 <st10294145@vcconnect.edu.za>
2025-04-25 09:30:12 +0200

    Added custom columns to User class.

--- app/src/main/java/com/fake/money_lock/data/User.kt
+++ app/src/main/java/com/fake/money_lock/data/User.kt
@@ -1,13 +1,21 @@
 package com.fake.money_lock.data
 
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "user_table")
 data class User(
     @PrimaryKey(autoGenerate = true)
-    val id: Int, //Account ID
-    val name: String, //User name
-    val email: String,   //User email address
-    val password: String //User password
+    @ColumnInfo(name = "user_id") // Custom column name
+    val id: Int, // Account ID
+
+    @ColumnInfo(name = "user_name") // Custom column name
+    val name: String, // User name
+
+    @ColumnInfo(name = "user_email") // Custom column name
+    val email: String, // User email address
+
+    @ColumnInfo(name = "user_password") // Custom column name
+    val password: String // User password
 )
