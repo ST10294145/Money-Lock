484510c7919542f42a118603a28ff267fab305ad
ST10294145 <st10294145@vcconnect.edu.za>
2025-05-01 12:19:13 +0200

    Fixed rendering error found in activity_login.xml

--- app/src/main/java/com/fake/money_lock/Login.kt
+++ app/src/main/java/com/fake/money_lock/Login.kt
@@ -11,6 +11,8 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+
+        // Apply system insets (status bar, navigation bar padding)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
