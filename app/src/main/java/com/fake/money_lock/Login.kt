3013310e6f84601f65e9cb1d3ca750fe10e5b97f
ST10294145 <st10294145@vcconnect.edu.za>
2025-05-01 12:49:48 +0200

    Implement basic login screen layout and logic binding for user input fields and button

--- app/src/main/java/com/fake/money_lock/Login.kt
+++ app/src/main/java/com/fake/money_lock/Login.kt
@@ -1,12 +1,20 @@
 package com.fake.money_lock
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class Login : AppCompatActivity() {
+
+    private lateinit var userId: EditText
+    private lateinit var password: EditText
+    private lateinit var login: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -17,6 +25,16 @@ class Login : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Bind UI elements
+        userId = findViewById(R.id.etLoginEmail)
+        password = findViewById(R.id.etLoginPassword)
+        login = findViewById(R.id.btnLogin)
+
+        // Handle login button click
+        login.setOnClickListener {
+
         }
     }
 }
