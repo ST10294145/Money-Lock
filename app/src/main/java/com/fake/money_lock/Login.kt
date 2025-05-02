7f354874f2dddf97f9ed48fea98fc35e7db9a520
ST10294145 <st10294145@vcconnect.edu.za>
2025-05-02 22:26:49 +0200

    Updated

--- app/src/main/java/com/fake/money_lock/Login.kt
+++ app/src/main/java/com/fake/money_lock/Login.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
+import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.lifecycleScope
 import com.fake.money_lock.data.UserDatabase
@@ -18,7 +18,8 @@ import kotlinx.coroutines.withContext
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        // Enable edge-to-edge the traditional way
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContentView(R.layout.activity_login)
 
         // Apply system insets
