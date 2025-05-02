9ee30aa3626165f5139ffaa1e891b74b6223b676
ST10294145 <st10294145@vcconnect.edu.za>
2025-05-02 18:17:54 +0200

    Created BudgetGoalEntity.kt

--- app/src/main/java/com/fake/money_lock/data/BudgetGoalEntity.kt
+++ app/src/main/java/com/fake/money_lock/data/BudgetGoalEntity.kt
@@ -1,4 +1,27 @@
 package com.fake.money_lock.data
 
-class BudgetGoalEntity {
-}
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "budget_goal_table")
+data class BudgetGoalEntity(
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int = 0,
+
+    @ColumnInfo(name = "user_id")
+    val userId: Int,
+
+    @ColumnInfo(name = "month")
+    val month: String, // Format: "2025-05"
+
+    @ColumnInfo(name = "monthly_budget")
+    val monthlyBudget: Double,
+
+    @ColumnInfo(name = "savings_goal")
+    val savingsGoal: Double,
+
+    @ColumnInfo(name = "description")
+    val description: String // Added description field
+)
